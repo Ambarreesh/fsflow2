019ace15b3effc29339b2c76f8f11e4ef8c99a58
Ambarreesh <ambarreeshrajprabu@gmail.com>
2026-06-15 12:58:37 +0530

    Update Google Drive download links for macOS and Windows

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -17,8 +17,8 @@ export default function Hero() {
 
   const handleDownload = (platform) => {
     const url = platform === 'macOS'
-      ? 'https://drive.google.com/file/d/1W8OkbAUGnk-wwISnhGYpzxH11qydcRAp/view?usp=sharing'
-      : 'https://drive.google.com/file/d/1Ius9ddbJzv3tCptER9cRWZNAAD7o1X4L/view?usp=sharing'
+      ? 'https://drive.google.com/file/d/1cTSKKEslSMSQbkBRT1NH_wh4JSPcJcQX/view?usp=sharing'
+      : 'https://drive.google.com/file/d/1SFCYlMITYDN-_C8poauq3ox5wqZS6Nmc/view?usp=sharing'
     window.open(url, '_blank')
   }
 
